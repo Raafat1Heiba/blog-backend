e951267f00f1a19de3030c8f1b3cd9ed79d1cb2c
Raafat1Heiba <raafatheiba6@gmail.com>
2024-04-18 09:15:57 +0200

    fix bugs

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "../backend/routes/userRoutes.js";
-import postRoutes from "../backend/routes/postRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 
